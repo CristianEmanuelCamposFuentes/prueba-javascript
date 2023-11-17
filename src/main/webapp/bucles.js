afbd47493b14de908b62658e6eae87ed065cb14d
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-11-16 21:57:07 -0300

    creando variables.js y bucles.js

---
+++ src/main/webapp/bucles.js
@@ -0,0 +1,11 @@
+// Bucle For
+for (let i = 1; i <= 5; i++) {
+    console.log("Iteración", i);
+}
+
+// Bucle While
+let contador = 0;
+while (contador < 3) {
+    console.log("Contador:", contador);
+    contador++;
+}
---
+++ src/main/webapp/variables.js
@@ -0,0 +1,18 @@
+// Declaración de variables
+var nombre = "Juan";
+// String nombre = "Nombre"
+let edad = 25;
+//console.log(nombre + edad)
+
+const PI = 3.1416;
+
+// Tipos de datos
+let isEstudiante = true;
+let listaDeNumeros = [1, 2, 3, 4, 5];
+
+// Imprimir en consola
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("PI:", PI);
+console.log("¿Es estudiante?", isEstudiante);
+console.log("Lista de números:", listaDeNumeros);
