5702c16f52e1d17e1ee389030357174dc277d266
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-11-23 20:46:20 -0300

    creando ejercicio de promesas.html

--- src/main/webapp/mostrarInfoEnWeb.js
+++ src/main/webapp/mostrarInfoEnWeb.js
@@ -1,11 +1,29 @@
 function mostrarInformacion() {
-    // Obtener el valor del nombre y edad
-    let nombre = document.getElementById("nombre").value;
-    let edad = document.getElementById("edad").value;
+    // Obtener los valores del nombre y la edad
+    var nombre = document.getElementById("nombre").value;
+    var edad = document.getElementById("edad").value;
 
-    // Construir el mensaje
-    let mensaje = "Nombre: " + nombre + "<br>Edad: " + edad;
+    // Crear una promesa
+    var promesa = new Promise(function(resolve, reject) {
+        // Simular una operación asincrónica (puedes reemplazar esto con una solicitud Ajax, por ejemplo)
+        setTimeout(function() {
+            // Verificar si se ingresaron ambos valores
+            if (nombre && edad) {
+                // Resuelve la promesa con la información
+                resolve({ nombre: nombre, edad: edad });
+            } else {
+                // Rechaza la promesa si falta información
+                reject("Por favor, ingresa tanto el nombre como la edad.");
+            }
+        }, 1000); // Simula una operación asincrónica de 1 segundo
+    });
 
-    // Mostrar el mensaje en el div
-    document.getElementById("resultado").innerHTML = mensaje;
+    // Manejar el resultado de la promesa
+    promesa.then(function(info) {
+        // Éxito: mostrar la información
+        document.getElementById("resultado").innerHTML = `Nombre: ${info.nombre}, <br> Edad: ${info.edad}`;
+    }).catch(function(error) {
+        // Error: mostrar el mensaje de error
+        document.getElementById("resultado").innerHTML = `Error: ${error}`;
+    });
 }
